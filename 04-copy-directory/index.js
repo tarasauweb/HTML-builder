73fc46de766c9886e34ac73c23c9c15fabb9b24e
Artur Tarasau <tarasov.a.web@gmail.com>
2023-05-05 20:47:26 +0300

    complite task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+const promise = fs.promises;
 
+function copyDir() {
+  promise.mkdir('files-copy' ,{recursive:true})
+  promise.readdir('files-copy').then((files)=>{
+    files.forEach(file=>{
+      promise.unlink(path.join(__dirname , 'files-copy' , file))
+    })
+  })
+  promise.readdir(path.join(__dirname,'files')).then((files)=>{
+    files.forEach(file=>{
+      promise.copyFile(path.join(__dirname , 'files' , file) , path.join(__dirname , 'files-copy' , file))
+    })
+  }).then(()=>{
+    console.log(`Copy complited ... `)
+  })
+  
+  
+}
+
+copyDir();
